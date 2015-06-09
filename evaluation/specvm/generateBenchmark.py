19cb52a05be91d60017c33398eae9182b1e2631e
mohlerm <github@mohlerm.ch>
2015-06-09 17:14:23 +0200

    further work on report

--- evaluation/specvm/generateBenchmark.py
+++ evaluation/specvm/generateBenchmark.py
@@ -3,7 +3,16 @@
 array = ['compress', 'crypto.aes', 'crypto.rsa', 'crypto.signverify', 'derby', 'mpegaudio', 'scimark.fft.large', 'scimark.lu.large', 'scimark.sor.large', 'scimark.sparse.large', 'scimark.fft.small', 'scimark.lu.small', 'scimark.sor.small', 'scimark.sparse.small', 'scimark.monte_carlo', 'serial', 'sunflow', 'xml.transform xml.validation']
 oldsum = 0
 for bench in array:
-    print('sh run-benchmark.sh baseline '+ bench + ' | tee warmupbench/'+ bench + '.baseline')
-    print('sh run-benchmark.sh create '+ bench + ' | tee warmupbench/'+ bench + '.create')
-    print('sh run-benchmark.sh use '+ bench + ' | tee warmupbench/'+ bench + '.use')
+    for i in range(1,31):
+        if i<10:
+            print('sh run-benchmark.sh baseline '+ bench + ' | tee singlebench/'+ bench + '.baseline.0'+str(i))
+        else:
+            print('sh run-benchmark.sh baseline '+ bench + ' | tee singlebench/'+ bench + '.baseline.'+str(i))
+    print('sh run-benchmark.sh create '+ bench + ' | tee singlebench/'+ bench + '.use')
+    for i in range(1,31):
+        if i<10:
+            print('sh run-benchmark.sh use '+ bench + ' | tee singlebench/'+ bench + '.use.0'+str(i))
+        else:
+            print('sh run-benchmark.sh use '+ bench + ' | tee singlebench/'+ bench + '.use.'+str(i))
+
 
